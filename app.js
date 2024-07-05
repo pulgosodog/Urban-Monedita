a9f662cebb8f664c415cc00705240b8c7e5ae4f5
Gabriel Y. <gabriel.yrigoyen43@gmail.com>
2024-07-05 11:45:05 -0600

    Made progress conecting node with ms sqlserver

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-const express = require('express');
-var bodyParser = require('body-parser');
-const path = require('path');
+import express from 'express';
+import bodyParser from 'body-parser';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 
+//----------------------------------------------------------------
+
 app.get('/', (req, res) => {
 res.render('index.ejs');
 });
 
 app.get('/viajes', (req, res) => {
-    res.render("viaje.ejs",{title:'Viajes'});
+    res.render("viaje.ejs", {title:'Viajes'});
 });
+
 app.get('/ingresos', (req, res) => {
-    res.render("index.ejs", {title:'Ingresos'});
+    res.render('ingresos.ejs',{title:'Ingresos'});
 });
 app.get('/conductores', (req, res) => {
     res.render("conductores.ejs", {title:'Conductores'});
